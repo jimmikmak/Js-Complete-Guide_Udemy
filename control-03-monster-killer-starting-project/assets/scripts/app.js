3d1f0781fd7bcaf6e3d717a70080617b4ccdae11
James McCarron <mccarron.j@gmail.com>
2021-05-27 11:39:09 +1000

    new strong attack function

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -9,6 +10,20 @@ adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentMonsterHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!!");
+  }
+}
+
+function strongAttackHandler() {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentMonsterHealth -= playerDamage;
