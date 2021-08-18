9aa1e4bfe95606e74489de1d87f89988a5d05d9d
James McCarron <mccarron.j@gmail.com>
2021-08-18 16:55:44 +1000

    While and do-while loop examples

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -215,9 +215,19 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  for (let i = 0; i < 3; i++) {
-    console.log("-----------");
-  }
+  // for (let i = 0; i < 3; i++) {
+  //   console.log("-----------");
+  // }
+  // let j = 0;
+  // while (j < 3) {
+  //   console.log(j);
+  //   j++;
+  // }
+  let j = 0;
+  do {
+    console.log(j);
+    j++;
+  } while (j < 3);
   // for (let i = 10; i > 0; i--) {
   //   console.log(i);
   // }
