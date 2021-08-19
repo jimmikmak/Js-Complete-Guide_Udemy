e6d4e7ae67390442a95ed4364b04db9f6bff6a8a
James McCarron <mccarron.j@gmail.com>
2021-08-19 15:37:45 +1000

    Block scope examples

---
+++ behind-scenes-block-scope/app.js
@@ -0,0 +1,16 @@
+let name = "James";
+
+if (name === "James") {
+  let hobbies = ["Sports", "Cooking"];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = "Clem";
+  console.log(name, age);
+}
+
+console.log(name, hobbies);
+
+greet();
