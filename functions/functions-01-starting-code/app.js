30eb78d34ebef8671c6110f534ef36fb52b6cbf8
James McCarron <mccarron.j@gmail.com>
2021-08-20 12:45:57 +1000

    Functions vs methods examples

--- functions/functions-01-starting-code/app.js
+++ functions/functions-01-starting-code/app.js
@@ -1,1 +1,16 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("Game is starting...");
+}
+
+const person = {
+  name: "James",
+  greet: function greet() {
+    console.log("Hello there!");
+  },
+};
+
+person.greet();
+
+startGameBtn.addEventListener("click", startGame);
