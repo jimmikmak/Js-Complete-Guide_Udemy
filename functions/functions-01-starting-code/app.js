d8eb10a3f5fa31a0f3cab8923e92f2e4fed05baf
James McCarron <mccarron.j@gmail.com>
2021-08-22 11:22:14 +1000

    Rest operator examples

--- functions/functions-01-starting-code/app.js
+++ functions/functions-01-starting-code/app.js
@@ -84,3 +84,27 @@ startGameBtn.addEventListener("click", () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to the game
+
+const sumUp = (...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+// pre ES6
+
+const subtractUp = function () {
+  let sum = 0;
+  for (const num of arguments) {
+    sum -= num;
+  }
+  return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
