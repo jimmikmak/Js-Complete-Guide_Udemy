53b3993ae2c6a523199b2ccd9de56bd42f535c05
James McCarron <mccarron.j@gmail.com>
2021-08-27 11:12:28 +1000

    Creating arrays

---
+++ arrays/arrays-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// less common ways of creating arrays
+
+// const moreNumbers = new Array(5, 2);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 3);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
