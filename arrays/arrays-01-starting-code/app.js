20987a129819e2a766d7e9605bb4afd2c2431342
James McCarron <mccarron.j@gmail.com>
2021-08-30 11:37:41 +1000

    Added array destructuring examples

--- arrays/arrays-01-starting-code/app.js
+++ arrays/arrays-01-starting-code/app.js
@@ -93,82 +93,89 @@
 
 // console.log(taxAdjustedPrices);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const taxAdjustedPrices = prices.map((price, idx, prices) => {
-  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-  return priceObj;
-});
-
-// console.log(prices, taxAdjustedPrices);
-const sortedPrices = prices.sort((a, b) => {
-  if (a > b) {
-    return 1;
-  } else if (a === b) {
-    return 0;
-  } else {
-    return -1;
-  }
-});
-
-// console.log(sortedPrices.reverse());
-console.log(sortedPrices);
-
-// shortest ver!!
-const filteredArray = prices.filter((p) => p > 6);
-
-console.log(filteredArray);
-
-// let sum = 0;
-
-// prices.forEach((price) => {
-//   sum += price;
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   return priceObj;
 // });
+
+// // console.log(prices, taxAdjustedPrices);
+// const sortedPrices = prices.sort((a, b) => {
+//   if (a > b) {
+//     return 1;
+//   } else if (a === b) {
+//     return 0;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// // console.log(sortedPrices.reverse());
+// console.log(sortedPrices);
+
+// // shortest ver!!
+// const filteredArray = prices.filter((p) => p > 6);
+
+// console.log(filteredArray);
+
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //   sum += price;
+// // });
+
+// // console.log(sum);
+
+// const sum = prices.reduce((preValue, curValue) => preValue + curValue, 0);
 
 // console.log(sum);
 
-const sum = prices.reduce((preValue, curValue) => preValue + curValue, 0);
-
-console.log(sum);
-
-// combining map() and reduce()
-const originalArray = [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }];
-const transformedArray = originalArray.map((obj) => obj.price); // produces [10.99, 5.99, 29.99]
-// const sum = transformedArray.reduce((sumVal, curVal) => sumVal + curVal, 0); // => 46.97
-
-// adding extraction logic with reduce()
+// // combining map() and reduce()
 // const originalArray = [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }];
-// const sum = originalArray.reduce((sumVal, curVal) => sumVal + curVal.price, 0); // => 46.97
+// const transformedArray = originalArray.map((obj) => obj.price); // produces [10.99, 5.99, 29.99]
+// // const sum = transformedArray.reduce((sumVal, curVal) => sumVal + curVal, 0); // => 46.97
 
-// split() & join() methods ->
-const data = "new york;10.99;2000";
+// // adding extraction logic with reduce()
+// // const originalArray = [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }];
+// // const sum = originalArray.reduce((sumVal, curVal) => sumVal + curVal.price, 0); // => 46.97
 
-const transformedData = data.split(";");
-transformedData[1] = +transformedData[1];
-console.log(transformedData);
+// // split() & join() methods ->
+// const data = "new york;10.99;2000";
 
-const nameFragments = ["Jimmy", "Mac"];
-const name = nameFragments.join(" ");
-console.log(name);
+// const transformedData = data.split(";");
+// transformedData[1] = +transformedData[1];
+// console.log(transformedData);
 
-// the spread operator
-const copiedNamedFragments = [...nameFragments];
-nameFragments.push("Mr");
-console.log(nameFragments, copiedNamedFragments);
+// const nameFragments = ["Jimmy", "Mac"];
+// const name = nameFragments.join(" ");
+// console.log(name);
 
-console.log(Math.min(...prices));
+// // the spread operator
+// const copiedNamedFragments = [...nameFragments];
+// nameFragments.push("Mr");
+// console.log(nameFragments, copiedNamedFragments);
 
-const persons = [
-  { name: "James", age: 43 },
-  { name: "Jimmy", age: 42 },
-];
-const copiedPersons = persons.map((person) => ({
-  name: person.name,
-  age: person.age,
-}));
+// console.log(Math.min(...prices));
 
-persons.push({ name: "Marina", age: 38 });
-persons[0].age = 44;
+// const persons = [
+//   { name: "James", age: 43 },
+//   { name: "Jimmy", age: 42 },
+// ];
+// const copiedPersons = persons.map((person) => ({
+//   name: person.name,
+//   age: person.age,
+// }));
 
-console.log(persons, copiedPersons);
+// persons.push({ name: "Marina", age: 38 });
+// persons[0].age = 44;
+
+// console.log(persons, copiedPersons);
+
+const nameData = ["Jimmy", "Mak", "Mr", 43];
+// const firstName = nameData[0];
+// const lastName = nameData[1];
+
+const [firstName, lastName, ...addInfo] = nameData;
+console.log(firstName, lastName, addInfo);
