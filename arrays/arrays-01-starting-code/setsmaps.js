0a93aec486b17a6ce354206c9cc34ee7d1323728
James McCarron <mccarron.j@gmail.com>
2021-08-30 15:31:55 +1000

    Working with Maps

--- arrays/arrays-01-starting-code/setsmaps.js
+++ arrays/arrays-01-starting-code/setsmaps.js
@@ -1,7 +1,35 @@
-const ids = new Set(["Hi", "from", "set!"]);
-ids.add(2);
-ids.delete("Hi");
+// const ids = new Set(["Hi", "from", "set!"]);
+// ids.add(2);
+// ids.delete("Hi");
 
-for (const entry of ids.values()) {
-  console.log(entry);
+// for (const entry of ids.values()) {
+//   console.log(entry);
+// }
+
+const person1 = { name: "James" };
+const person2 = { name: "Marina" };
+
+const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+
+personData.set(person2, [{ date: "two weeks ago", price: 50 }]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+// for (const entry of personData.entries()) {
+//   console.log(entry);
+// }
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
 }
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+for (const value of personData.values()) {
+  console.log(value);
+}
+
+console.log(personData.size);
