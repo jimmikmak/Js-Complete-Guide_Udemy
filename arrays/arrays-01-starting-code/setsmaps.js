4a45331c04c106e85b2fd41a53641bea95ba45f4
James McCarron <mccarron.j@gmail.com>
2021-08-30 16:19:32 +1000

    WeakSet example

--- arrays/arrays-01-starting-code/setsmaps.js
+++ arrays/arrays-01-starting-code/setsmaps.js
@@ -6,30 +6,39 @@
 //   console.log(entry);
 // }
 
-const person1 = { name: "James" };
-const person2 = { name: "Marina" };
+// const person1 = { name: "James" };
+// const person2 = { name: "Marina" };
 
-const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+// const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
 
-personData.set(person2, [{ date: "two weeks ago", price: 50 }]);
+// personData.set(person2, [{ date: "two weeks ago", price: 50 }]);
 
-console.log(personData);
-console.log(personData.get(person1));
+// console.log(personData);
+// console.log(personData.get(person1));
 
-// for (const entry of personData.entries()) {
-//   console.log(entry);
+// // for (const entry of personData.entries()) {
+// //   console.log(entry);
+// // }
+
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
 // }
 
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
 
-for (const key of personData.keys()) {
-  console.log(key);
-}
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
 
-for (const value of personData.values()) {
-  console.log(value);
-}
+// console.log(personData.size);
 
-console.log(personData.size);
+let person = { name: "James" };
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operations
+person = null;
+
+console.log(persons);
