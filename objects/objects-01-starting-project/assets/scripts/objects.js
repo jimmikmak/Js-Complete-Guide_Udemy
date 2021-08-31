01b2bae876b05eb46e935a9d60f15cbdd3b8fd19
James McCarron <mccarron.j@gmail.com>
2021-08-31 13:57:40 +1000

    Dynamic property access & setting properties

--- objects/objects-01-starting-project/assets/scripts/objects.js
+++ objects/objects-01-starting-project/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById("movie-list");
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
 
+const userChosenKeyName = "level";
+
 let person = {
   "first name": "James",
   age: 43,
   hobbies: ["Swimming", "Scuba Diving"],
+  [userChosenKeyName]: "...",
   greet: function () {
     alert("Hi there!");
   },
@@ -19,6 +22,8 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person["first name"]);
+const keyName = "first name";
+
+console.log(person[keyName]);
 console.log(person[1.5]);
 console.log(person);
