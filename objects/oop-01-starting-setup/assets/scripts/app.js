bab4d14444314d3ac29ad44f425ac6df5c40c851
James McCarron <mccarron.j@gmail.com>
2021-09-07 15:59:08 +1000

    Two different ways of adding methods: ShoppingCart Component

--- objects/oop-01-starting-setup/assets/scripts/app.js
+++ objects/oop-01-starting-setup/assets/scripts/app.js
@@ -62,7 +62,12 @@ class ShoppingCart extends Component {
   }
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Ordering...");
+      console.log(this.items);
+    };
+    this.render();
   }
 
   addProduct(product) {
@@ -77,6 +82,9 @@ class ShoppingCart extends Component {
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
+    const orderButton = cartEl.querySelector("button");
+    // orderButton.addEventListener("click", () => this.orderProducts());
+    orderButton.addEventListener("click", this.orderProducts);
     this.totalOutput = cartEl.querySelector("h2");
   }
 }
