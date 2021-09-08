a26a62e3b69de186c58a22a16559d57ed51dc4be
James McCarron <mccarron.j@gmail.com>
2021-09-08 12:14:24 +1000

    OOP assignment completed

--- objects/oop-assignment-problem/assignment.js
+++ objects/oop-assignment-problem/assignment.js
@@ -1,13 +1,15 @@
 class Course {
+  #price;
+
   get price() {
-    return "$" + this._price;
+    return "$" + this.#price;
   }
 
   set price(value) {
     if (value < 0) {
       throw "Invalid value!!";
     }
-    this._price = value;
+    this.#price = value;
   }
 
   constructor(courseTitle, courseLength, coursePrice) {
@@ -17,7 +19,7 @@ class Course {
   }
 
   calculateValue() {
-    return this.length / this._price;
+    return this.length / this.#price;
   }
 
   printSummary() {
@@ -68,7 +70,7 @@ const flutterCourse = new TheoreticalCourse(
   80
 );
 
-flutterCourse.price = 100;
+flutterCourse.price = 50;
 
 flutterCourse.printSummary();
 flutterCourse.publish();
