de6d3e2201df06b165ba7e966a40fff3cc834ca6
James McCarron <mccarron.j@gmail.com>
2021-09-15 11:23:04 +1000

    Factory function examples

--- functions/adv-fn-01-starting-setup/app.js
+++ functions/adv-fn-01-starting-setup/app.js
@@ -28,3 +28,17 @@ const printHobbies = (h) => {
 };
 
 printHobbies(hobbies);
+
+const createTaxCalculator = (tax) => {
+  const calculateTax = (amount) => {
+    return amount * tax;
+  };
+
+  return calculateTax;
+};
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
