2ea4ed9f475cbeb367c76257c4057c31c6570a8e
James McCarron <mccarron.j@gmail.com>
2021-09-15 15:55:09 +1000

    Closures in practice

--- functions/adv-fn-01-starting-setup/app.js
+++ functions/adv-fn-01-starting-setup/app.js
@@ -29,9 +29,12 @@ const printHobbies = (h) => {
 
 printHobbies(hobbies);
 
+let multiplier = 1.1;
+
 const createTaxCalculator = (tax) => {
   const calculateTax = (amount) => {
-    return amount * tax;
+    console.log(multiplier);
+    return amount * tax * multiplier;
   };
 
   return calculateTax;
@@ -40,5 +43,20 @@ const createTaxCalculator = (tax) => {
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
+// multiplier = 1.2;
+
 console.log(calculateVatAmount(100));
 console.log(calculateVatAmount(200));
+
+let userName = "James";
+
+const greetUser = () => {
+  let name = "Clem";
+  console.log("Hi " + name + "!");
+};
+
+// let name = Mac;
+
+userName = "Marina";
+
+greetUser();
