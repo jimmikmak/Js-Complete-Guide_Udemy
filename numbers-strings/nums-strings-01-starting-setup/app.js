ab0d11202824d383cf651a03677eb7e859631026
James McCarron <mccarron.j@gmail.com>
2021-09-17 12:58:17 +1000

    Tagged template example

--- numbers-strings/nums-strings-01-starting-setup/app.js
+++ numbers-strings/nums-strings-01-starting-setup/app.js
@@ -5,3 +5,22 @@ const randomIntBetween = (min, max) => {
 };
 
 console.log(randomIntBetween(1, 10));
+
+const productDescription = (strings, productName, productPrice) => {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = "pretty cheap regarding its price";
+  if (productPrice > 20) {
+    priceCategory = "fairly priced";
+  }
+  // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+  return { name: productName, price: productPrice };
+};
+
+const prodName = "JavaScript Course";
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}`;
+
+console.log(productOutput);
