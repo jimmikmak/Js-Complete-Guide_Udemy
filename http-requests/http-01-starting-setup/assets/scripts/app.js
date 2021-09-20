b405f238703313bf9f669c22702ef9da14dab3b9
James McCarron <mccarron.j@gmail.com>
2021-09-20 16:58:42 +1000

    Sending a DELETE request

--- http-requests/http-01-starting-setup/assets/scripts/app.js
+++ http-requests/http-01-starting-setup/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 const sendHttpRequest = (method, url, data) => {
   const promise = new Promise((resolve, reject) => {
@@ -33,6 +34,7 @@ const fetchPosts = async () => {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
 };
@@ -57,5 +59,12 @@ form.addEventListener("submit", (event) => {
   createPost(enteredTitle, enteredContent);
 });
 
-fetchPosts();
-createPost("DUMMY", "A dummy post!");
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
