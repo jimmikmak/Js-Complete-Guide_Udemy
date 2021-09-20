352c3f2b0430636108232f9aa37532e54e24318a
James McCarron <mccarron.j@gmail.com>
2021-09-20 15:03:52 +1000

    Promisifying the GET request

--- http-requests/http-01-starting-setup/assets/scripts/app.js
+++ http-requests/http-01-starting-setup/assets/scripts/app.js
@@ -1,16 +1,39 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+const sendHttpRequest = (method, url) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.open(method, url);
 
-xhr.responseType = "json";
+    xhr.responseType = "json";
 
-xhr.onload = function () {
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
-  console.log(listOfPosts);
+    xhr.onload = function () {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+      const listOfPosts = xhr.response;
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        listElement.append(postEl);
+      }
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+};
+
+const fetchPosts = async () => {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
@@ -19,4 +42,4 @@ xhr.onload = function () {
   }
 };
 
-xhr.send();
+fetchPosts();
