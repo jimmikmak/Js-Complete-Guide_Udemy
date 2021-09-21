dbcf6763b768f6efa61ca00d7e0e02bb6ba2383a
James McCarron <mccarron.j@gmail.com>
2021-09-21 12:35:27 +1000

    Added request headers

--- http-requests/http-01-starting-setup/assets/scripts/app.js
+++ http-requests/http-01-starting-setup/assets/scripts/app.js
@@ -7,6 +7,8 @@ const postList = document.querySelector("ul");
 const sendHttpRequest = (method, url, data) => {
   // const promise = new Promise((resolve, reject) => {
   //   const xhr = new XMLHttpRequest();
+  // xhr.setRequestHeader("Content-Type", "application/json");
+
   //   xhr.open(method, url);
   //   xhr.responseType = "json";
   //   xhr.onload = function () {
@@ -28,6 +30,9 @@ const sendHttpRequest = (method, url, data) => {
   return fetch(url, {
     method: method,
     body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
   }).then((response) => {
     return response.json();
   });
