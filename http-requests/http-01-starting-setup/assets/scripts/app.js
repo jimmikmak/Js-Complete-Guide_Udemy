a07ffb17a67b2d3b3bc9c1f03d9819d42febc0fc
James McCarron <mccarron.j@gmail.com>
2021-09-21 13:52:01 +1000

    FormData example with extra tag ids

--- http-requests/http-01-starting-setup/assets/scripts/app.js
+++ http-requests/http-01-starting-setup/assets/scripts/app.js
@@ -29,10 +29,10 @@ const sendHttpRequest = (method, url, data) => {
   // return promise;
   return fetch(url, {
     method: method,
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
+    body: data,
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
   })
     .then((response) => {
       if (response.status >= 200 && response.status < 300) {
@@ -78,7 +78,12 @@ const createPost = async (title, content) => {
     userId: userId,
   };
 
-  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+  const fd = new FormData(form);
+  // fd.append("title", title);
+  // fd.append("body", content);
+  fd.append("userId", userId);
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", fd);
 };
 
 fetchButton.addEventListener("click", fetchPosts);
