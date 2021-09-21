3ef43bb5f5683b1891526997193ba78cbdee16c4
James McCarron <mccarron.j@gmail.com>
2021-09-21 12:03:15 +1000

    POSTing data with fetch

--- http-requests/http-01-starting-setup/assets/scripts/app.js
+++ http-requests/http-01-starting-setup/assets/scripts/app.js
@@ -25,7 +25,10 @@ const sendHttpRequest = (method, url, data) => {
   // });
 
   // return promise;
-  return fetch(url).then((response) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+  }).then((response) => {
     return response.json();
   });
 };
