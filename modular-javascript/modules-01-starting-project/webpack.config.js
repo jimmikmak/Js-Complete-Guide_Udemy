8c83f2064f1d6971fd524b07dcafd6c92f97a982
James McCarron <mccarron.j@gmail.com>
2021-09-23 13:06:56 +1000

    Webpack-dev-server path error unresolved

--- modular-javascript/modules-01-starting-project/webpack.config.js
+++ modular-javascript/modules-01-starting-project/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "assets", "scripts"),
+    publicPath: "assets/scripts/",
   },
+  // devServer: {
+  //   port: 3000,
+  //   writeToDisk: true,
+  //   proxy: {
+  //     "/": "http://localhost:8080",
+  //   },
+  // },
 };
