311592f10aff58b5e0a2f40224726f7032edb61a
James McCarron <mccarron.j@gmail.com>
2021-09-25 11:11:22 +1000

    Cookies example

---
+++ browser-storage/storage-01-starting-setup/app.js
@@ -0,0 +1,11 @@
+const storeBtn = document.getElementById("store-btn");
+const retrBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {
+  const userId = "u123";
+  document.cookie = `uid = ${userId}`;
+});
+
+retrBtn.addEventListener("click", () => {
+  console.log(document.cookie);
+});
