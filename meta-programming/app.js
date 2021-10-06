bd35ee0d82c26cf3ecae06f72766fa21835009b5
James McCarron <mccarron.j@gmail.com>
2021-10-06 16:49:41 +1100

    Iterators intro

--- meta-programming/app.js
+++ meta-programming/app.js
@@ -18,3 +18,25 @@ user.id = "p2"; // this should not be possible!
 console.log(user[Symbol("uid")]);
 console.log(Symbol("uid") === Symbol("uid"));
 console.log(user.toString());
+
+const company = {
+  curEmployee: 0,
+  employees: ["James", "Tim", "Marina"],
+  next() {
+    if (this.curEmployee >= this.employees.length) {
+      return { value: this.curEmployee, done: true };
+    }
+    const returnValue = {
+      value: this.employees[this.curEmployee],
+      done: false,
+    };
+    this.curEmployee++;
+    return returnValue;
+  },
+};
+
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
+console.log(company.next());
