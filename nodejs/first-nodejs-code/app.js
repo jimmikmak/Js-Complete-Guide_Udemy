f7e5f2ca2596dfdb9ffd1e21e52be10ab27650ce
James McCarron <mccarron.j@gmail.com>
2021-10-08 13:21:09 +1100

    Module example & file access

--- nodejs/first-nodejs-code/app.js
+++ nodejs/first-nodejs-code/app.js
@@ -1,3 +1,17 @@
-const userName = "James";
+const fs = require("fs");
 
-console.log(`Hi ${userName}!`);
+fs.readFile("user-data.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString());
+});
+
+fs.writeFile("user-data.txt", "username=James", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Wrote to file!");
+  }
+});
