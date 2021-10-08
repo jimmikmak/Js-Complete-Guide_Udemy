d2718fa42df7408609d5acff8c2c3d283c826a2a
James McCarron <mccarron.j@gmail.com>
2021-10-08 13:52:24 +1100

    Parsing incoming data

--- nodejs/first-nodejs-code/app.js
+++ nodejs/first-nodejs-code/app.js
@@ -1,9 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello there!</h1>");
-  res.end();
+  let body = [];
+  req.on("data", (chunk) => {
+    body.push(chunk);
+  });
+  req.on("end", () => {
+    body = Buffer.concat(body).toString();
+    let userName = "Unknown user";
+    if (body) {
+      userName = body.split("=")[1];
+    }
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
+    );
+    res.end();
+  });
 });
 
 server.listen(3000);
