e5d39041150a9caaf87db428b59951d71bb72749
James McCarron <mccarron.j@gmail.com>
2021-10-12 17:01:28 +1100

    Added more access controls

--- nodejs/first-nodejs-code/app.js
+++ nodejs/first-nodejs-code/app.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
