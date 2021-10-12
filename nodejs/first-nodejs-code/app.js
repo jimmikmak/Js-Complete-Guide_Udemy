6972736252111eee69e3e8916abd8a2521cf0130
James McCarron <mccarron.j@gmail.com>
2021-10-12 15:45:59 +1100

    CORS

--- nodejs/first-nodejs-code/app.js
+++ nodejs/first-nodejs-code/app.js
@@ -1,23 +1,33 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
 
+const locationRoutes = require("./routes/location");
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+// app.set("view engine", "ejs");
+// app.set("views", "views");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET");
   next();
 });
 
-app.use((req, res, next) => {
-  let userName = req.body.username || "Unknown User";
-  res.render("index", {
-    user: userName,
-  });
-});
+app.use(locationRoutes);
+
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Type", "text/html");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   let userName = req.body.username || "Unknown User";
+//   res.render("index", {
+//     user: userName,
+//   });
+// });
 
 app.listen(3000);
