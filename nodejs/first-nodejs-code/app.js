57175fb0d688deb88109f7489d5bf1bd9861562b
James McCarron <mccarron.j@gmail.com>
2021-10-12 13:08:22 +1100

    Installed EJS & created new views folder

--- nodejs/first-nodejs-code/app.js
+++ nodejs/first-nodejs-code/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
